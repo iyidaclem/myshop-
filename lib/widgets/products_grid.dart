f1443b50d0faf2572d41cd16448a2e2595a19005
iyidaclem <iyidaclem@gmail.com>
2021-07-13 16:32:40 +0100

    changes made

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -8,29 +8,35 @@ class ProductsGrid extends StatelessWidget {
   final bool showFavs;
 
   ProductsGrid(this.showFavs);
+  Future<void> _refresh(BuildContext context)async {
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     final products = showFavs ? productsData.favoriteItems : productsData.items;
-    return GridView.builder(
-      
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-            // builder: (c) => products[i],
-            value: products[i],
-            child: ProductItem(
-                // products[i].id,
-                // products[i].title,
-                // products[i].imageUrl,
-                ),
-          ),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+    return RefreshIndicator(
+      onRefresh: ()=>_refresh(context),
+      child: GridView.builder(
+        
+        padding: const EdgeInsets.all(10.0),
+        itemCount: products.length,
+        itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              // builder: (c) => products[i],
+              value: products[i],
+              child: ProductItem(
+                  // products[i].id,
+                  // products[i].title,
+                  // products[i].imageUrl,
+                  ),
+            ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
       ),
     );
   }
